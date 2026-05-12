b131ae8900ff6f52d20f3557f1ea74012400fc43
onikhil361-cmd <onikhil361@gmail.com>
2026-05-12 23:23:43 +0530

    completed Task 4

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require("express");
+const fs= require("fs-extra")
+ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
-
 let users = [];
 
 app.set("view engine", "ejs");
@@ -15,26 +14,46 @@ app.get("/", (req, res) => {
     res.render("index", { error: "" });
 });
 
-app.post("/submit", (req, res) => {
+app.post("/submit", async (req, res) => {
 
     const { name, email, age } = req.body;
 
-    if (!name || !email || !age) {
+    if(!name || !email || !age){
 
         return res.render("index", {
-            error: "All fields are required!"
+            error: "All fields required"
         });
     }
 
-    users.push({ name, email, age });
+    if(age < 18){
+
+        return res.render("index", {
+            error: "Age must be 18+"
+        });
+    }
+
+    let users = await fs.readJson("data.json");
+
+    users.push({
+        name,
+        email,
+        age
+    });
+
+    await fs.writeJson("data.json", users);
 
     res.redirect("/users");
-});
 
-app.get("/users", (req, res) => {
+});
+app.get("/users", async (req, res) => {
+
+    const users = await fs.readJson("data.json");
+
     res.render("users", { users });
+
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
